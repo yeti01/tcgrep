ff537eea012c3eae04402c07f852689a6f20afc8
Ulrich Steinmann <usteinmann@gmx.de>
2023-08-12 10:53:44 +0200

    tcgrep.pl v1.2

--- tcgrep.pl
+++ tcgrep.pl
@@ -1,168 +1,251 @@
-=begin metadata
-
-Xref: feenix.metronet.com comp.unix.questions:11476
-Newsgroups: comp.unix.questions
-Path: feenix.metronet.com!news.utdallas.edu!wupost!howland.reston.ans.net!pipex!uunet!boulder!wraeththu.cs.colorado.edu!tchrist
-From: Tom Christiansen <tchrist@cs.Colorado.EDU>
-Subject: Re: grep with highlight capability anyone?
-Message-ID: <CE5G42.77u@Colorado.EDU>
-Originator: tchrist@wraeththu.cs.colorado.edu
-Sender: news@Colorado.EDU (USENET News System)
-Reply-To: tchrist@cs.colorado.edu (Tom Christiansen)
-Organization: University of Colorado, Boulder
-References: <1993Sep28.173028.27194@bellahs.com>
-Date: Thu, 30 Sep 1993 04:21:37 GMT
-Lines: 148
-
-From the keyboard of gfong@bellahs.com (Gary Fong RD):
-:UNIXians,
-:Does there exist a command as powerful as grep which
-:highlights/reversevideo/bolds the matching string/expression?
-
-I use the -H flag in the following program.  Usages
-Perl regexps, meaning more like egrep than grep. 
-See the usage for other features, like recursion, 
-match-once, textfile only, and paragraph mode.
-
---tom
-
-=end metadata
-
-=cut
-
-#!/usr/bin/perl
+#! /usr/bin/perl
 #
 # tcgrep: tom christiansen's rewrite of grep
 # tchrist@colorado.edu
 # see usage for features
 # yet to implement: -f
+# v1.0: Thu Sep 30 16:24:43 MDT 1993
+# v1.1: Fri Oct  1 08:33:43 MDT 1993
+#
+# Revision by Greg Bacon <gbacon@cs.uah.edu>
+# Fixed up highlighting for those of us trapped in terminfo
+# implemented -f
+# v1.2: Fri Jul 26 13:37:02 CDT 1996
 
-require 'getopts.pl';
-&Getopts("inqclse:f:xwhv1pHtr") || &usage;
-
-die "-f unsupported" if $opt_f;
-
-$Pattern = $opt_e || shift || &usage;
-
-eval { /$Pattern/, 1 } || die "$0: bad pattern: $@";
-
-if ($opt_H) {
-    $ospeed = 13;  # bogus but shouldn't hurt; means 9600
-    require 'termcap.pl';
-    &Tgetent($ENV{TERM} || 'vt100');
-}
-
-$opt_i && $Pattern =~ s/\w/[\u$&\l$&]/gi;
-$opt_p && ($/ = '');
-$opt_w && ($Pattern = '\b' . $Pattern . '\b');
-$opt_x && ($Pattern = "^$Pattern\$");
-$opt_1 += $opt_l;
-$mult = 1 if ($opt_r || @ARGV > 1) && !$opt_h;
-
-@ARGV = ($opt_r ? '.' : '-') unless @ARGV;
-
-$Errors = $Total = 0;
-
-&match(@ARGV);
-print $Total, "\n" if $opt_c;
+&init;
+&parse_args;
+&matchfile(@ARGV);
 
 exit(2) if $Errors;
-exit(0) if $Total;
+exit(0) if $Grand_Total;
 exit(1);
 
 ######################################
 
-sub match {
+sub init {
+    ($me = $0) =~ s!.*/!!;
+    $Errors = $Grand_Total = 0;
+    $| = 1;
+
+    %Compress = (
+        'z',    gzcat,
+        'gz',   gzcat,
+        'Z',    zcat,
+    );
+}
+
+sub matchfile {
     local($_,$file);
     local(@list);
     local($matches);
+    local($total);
 
-    #warn "match /$Pattern/ @_\n";
+FILE: while (defined ($file = shift(@_))) {
 
-FILE: 
-    while ($file = shift) {
+        if (-d $file) {
+            if (-l $file && @ARGV != 1) {
+                warn "$me: \"$file\" is a symlink to a directory\n"
+                    if $opt_T;
+                next FILE;
+                
+            } 
+            if (!$opt_r) {
+                warn "$me: \"$file\" is a directory, but no -r given\n"
+                    if $opt_T;
+                next FILE;
+            } 
+            if (!opendir(DIR, $file)) {
+                unless ($opt_q) {
+                    warn "$me: can't opendir $file: $!\n";
+                    $Errors++;
+                }
+                next FILE;
+            } 
+            @list = ();
+            for (readdir(DIR)) {
+                push(@list, "$file/$_") unless /^\.{1,2}$/;
+            } 
+            closedir(DIR);
+            if ($opt_t) {
+                local(@dates, $i);
+                for (@list) { push(@dates, -M) } 
+                @list = @list[sort { $dates[$a] <=> $dates[$b] } 0..$#dates];
+            } else {
+                @list = sort @list;
+            } 
+            &matchfile(@list);
+            next FILE;
+        } 
 
-	if (-d $file) {
-	    next FILE if -l $file;
-	    if (!$opt_r) {
-		warn "$0: $file is a directory, but no -r given\n";
-		next FILE;
-	    } 
-	    if (!opendir(DIR, $file)) {
-		unless ($opt_q) {
-		    warn "$0: can't opendir $file: $!\n";
-		    $Errors++;
-		}
-		next FILE;
-	    } 
-	    @list = ();
-	    for (sort readdir(DIR)) {
-		push(@list, "$file/$_") unless /^\.{1,2}$/;
-	    } 
-	    closedir(DIR);
-	    &match(@list);
-	    next FILE;
-	} 
+        if ($file eq '-') {
+            warn "$me: reading from stdin\n" if -t STDIN && !$opt_q;
+            $name = '<STDIN>';
+        } else {
+            $name = $file;
+            unless (-f $file || $opt_a) {
+                warn qq($me: skipping non-plain file "$file"\n) if $opt_T;
+                next FILE;
+            }
 
-	#warn "checking $file\n";
-	if (!open(FILE, $file)) {
-	    unless ($opt_q) {
-		warn "$0: $file: $!\n";
-		$Errors++;
-	    }
-	    next FILE;
-	} 
+            ($ext) = $file =~ /\.([^.]+)$/;
+            if ( $Compress{$ext} ) {
+                $file = "$Compress{$ext} <$file |";
+            } elsif (! (-T $file  || $opt_a)) {
+                warn qq($me: skipping binary file "$file"\n) if $opt_T;
+                next FILE;
+            }
+        }
 
-	if ($_ ne '-') {
-	    next FILE unless -f FILE || $opt_d;
-	    next FILE unless -T FILE || $opt_b;
-	} else {
-	    warn "$0: reading from stdin\n" if -t STDIN;
-	} 
-	$matches = 0;
-LINE:  
-	while (<FILE>) {
-	    $matches = 0;
-	    if ($opt_H) {
-		$matches = s/$Pattern/$TC{'so'}$&$TC{'se'}/go;
-	    } elsif ($opt_v) {
-		$matches = !/$Pattern/o;
-	    } else {
-		$matches++ while /$Pattern/go; 	
-	    }
-	    next LINE unless $matches;
-	    $Total += $matches;
-	    print "$FILE\n", next FILE if $opt_l;
-	    print $mult && "$file: ", $opt_n && "$.:", $_ unless $opt_s;
-	    next FILE if $opt_1;
-	} 
-    }
+        warn "$me: checking $file\n" if $opt_T;
+
+        if (!open(FILE, $file)) {
+            unless ($opt_q) {
+                warn "$me: $file: $!\n";
+                $Errors++;
+            }
+            next FILE;
+        } 
+        $total = 0;
+
+        $matches = 0;
+
+LINE:  while (<FILE>) {
+            $matches = 0;
+
+            study if @Patterns > 5;
+
+            if ($opt_H) {
+                for $Pattern (@Patterns) {
+		    $matches += s/$Pattern/${SO}$&${SE}/g;
+                }
+            } elsif ($opt_v) {
+                for $Pattern (@Patterns) {
+		    $matches += !/$Pattern/;
+                }
+            } elsif ($opt_C) {
+                for $Pattern (@Patterns) {
+		    $matches++ while /$Pattern/g;
+                }
+            } else {
+                for $Pattern (@Patterns) {
+		    $matches++ if /$Pattern/;
+                }
+            }
+
+            next LINE unless $matches;
+            $total += $matches;
+            if ($opt_p || $opt_P) {
+                local($*);
+                s/\n{2,}$/\n/ if $opt_p;
+                s,$/$,,o      if $opt_P;
+            } 
+            print("$name\n"), next FILE if $opt_l;
+            $opt_s || print $mult  && "$name:", 
+                            $opt_n && "$.:", 
+                            $_, 
+                            ($opt_p||$opt_P) && ('-' x 20)."\n";
+            next FILE if $opt_1;
+        }  
+    } continue {
+        print $mult  && "$name:", $total, "\n" if $opt_c;
+    } 
+    $Grand_Total += $total;
 }
 
 sub usage { 
     die <<EOF
-usage: $0 [flags] [files]
-    i	case insensitive 
-    n	number lines
-    c	give count of matches 
-    w 	word boundaries only
-    s	silent mode
+usage: $me [flags] [files]
+
+Standard grep options:
+    i   case insensitive 
+    n   number lines
+    c   give count of lines matching
+    C   ditto, but >1 match per line possible
+    w   word boundaries only
+    s   silent mode
     x   exact matches only
-    v	invert search sense (lines that DON'T match)
-    h	hide filenames
-    e	expression (for exprs beginning with -)
-    f	file with expressions [unimplemented]
-    l	list filenames matching
-    1	1 match per file 
-    r	recursive on directories or dot if none
-    p	paragraph mode (default: line mode)
-    b	binary files also (default: text only)
-    d	device and special files also (default: files only)
-    q	quiet about failed file and dir opens
-    H	highlight matches
+    v   invert search sense (lines that DON'T match)
+    h   hide filenames
+    e   expression (for exprs beginning with -)
+    f   file with expressions
+    l   list filenames matching
+
+Specials:
+    1   1 match per file 
+    H   highlight matches
+    u   underline matches
+    r   recursive on directories or dot if none
+    t   process directories in `ls -t` order
+    p   paragraph mode (default: line mode)
+    P   ditto, but specify separator, e.g. -P '%%\\n'
+    a   all files, not just plain text files 
+    q   quiet about failed file and dir opens
+    T   trace files as opened
 EOF
 }
--- 
-    Tom Christiansen      tchrist@cs.colorado.edu       
-		    Consultant
-	Boulder Colorado  303-444-3212
+
+sub parse_args {
+
+    require 'getopts.pl';
+
+    if ($_ = $ENV{TCGREP}) {
+        s/^[^\-]/-$&/;
+        unshift(@ARGV, $_);
+    } 
+
+    &Getopts("inqcClsue:f:xwhva1pHtrT-P:") || &usage;
+
+    if ($opt_f) {
+        open(PATFILE, $opt_f) || die qq($me: Can't open '$opt_f': $!);
+
+       # make sure perl is down with these patterns...
+        while ($pattern = <PATFILE>) {
+            chop $pattern;
+	    eval { /$pattern/, 1 } || die "$me: $opt_f:$.: bad pattern: $@";
+	    push @Patterns, $pattern;
+        }
+        close PATFILE;
+    } else {
+	$pattern = $opt_e || shift(@ARGV) || &usage;
+	eval { /$pattern/, 1 } || die "$me: bad pattern: $@";
+	@Patterns = ($pattern);
+    }
+
+    if ($opt_H || $opt_u) {
+        $ospeed = 13;  # bogus but shouldn't hurt; means 9600
+        require 'termcap.pl';
+        local($term) = ($ENV{TERM} || 'vt100');
+        &Tgetent($term);
+        ($SO, $SE) =  $opt_H ? @TC{'so','se'} : @TC{'us','ue'};
+
+        unless ($SO || $SE) {
+            ($SO, $SE) = $opt_H
+                  ?
+                  (`tput -T $term smso`, `tput -T $term rmso`)
+                  :
+                  (`tput -T $term smul`, `tput -T $term rmul`);
+        }
+    }
+
+    if ($opt_i) {
+        if ($] < 5) {
+            @Patterns = grep(s/\w/[\u$&\l$&]/gi, @Patterns);
+        } else {
+            @Patterns = grep($_ = "(?i)$_", @Patterns);
+        } 
+    }
+
+    $opt_p && ($/ = '', $* = 1);
+    $opt_P && ($/ = eval(qq("$opt_P")), $*=1); # for -P '%%\n'
+    $opt_w && (@Patterns = grep($_ = '\b' . $_ . '\b', @Patterns));
+    $opt_x && (@Patterns = grep($_ = "^$_\$", @Patterns));
+    $mult = 1 if ($opt_r || (@ARGV > 1) || -d $ARGV[0]) && !$opt_h;
+    $opt_1 += $opt_l;
+    $opt_H += $opt_u;
+    $opt_c += $opt_C;
+    $opt_s += $opt_c;
+    $opt_1 += $opt_s && !$opt_c;
+
+    @ARGV = ($opt_r ? '.' : '-') unless @ARGV;
+    $opt_r = 1 if !$opt_r && grep(-d, @ARGV) == @ARGV;
+}
